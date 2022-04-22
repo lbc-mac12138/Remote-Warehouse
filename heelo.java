b42b21b839b377880aeae76281a5e544881a1513
lbc-mac12138 <104205970+lbc-mac12138@users.noreply.github.com>
2022-04-22 21:04:06 +0800

    heel.java
    longcommit

--- heelo.java
+++ heelo.java
@@ -14,5 +14,15 @@ hu
 afhouafehioafijoadfsijoafoiafs:
 
 	gdshioadghiopudfgshiodfgigsdfhodgs
+		Three years ago, I accidentally realized that my handwriting looks sort of ugly; however, 
+	my best friend’s handwriting is real nice as an art. Then I have a passion for the beauty of handwriting,
+	so I decided to practice calligraphy. But the result of half-year practice was unsatisfied than expected. 
+		I was very frustrated and want to give up. My friend found my sadness about that,
+	she encouraged me and gave some advice like there was something wrong with my way to hold the pen. 
+		She corrected the holding position and encouraged me that I could do better while kept practicing. With her enthusiastic help, 
+	I stick at doing the calligraphy exercise and I have better handwriting than before. Actually, 
+	I find joy in it and the exercises become one of my habits and hobbies. 
+			Therefore the key to success consists in your persistence of the intention.
+
 }
 
