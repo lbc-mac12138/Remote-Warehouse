9ab85f774977e1eedfe17d96979cb5b8283fff2a
lbc-mac12138 <104205970+lbc-mac12138@users.noreply.github.com>
2022-04-22 21:12:23 +0800

    long2323232commit

--- heelo.java
+++ heelo.java
@@ -17,6 +17,7 @@ afhouafehioafijoadfsijoafoiafs:
 		Three years ago, I accidentally realized that my handwriting looks sort of ugly; however, 
 	my best friend’s handwriting is real nice as an art. Then I have a passion for the beauty of handwriting,
 	so I decided to practice calligraphy. But the result of half-year practice was unsatisfied than expected.
-
+	三年前，我偶然间意识到自己的书写有点难看，然而我发现我好友的书法简直漂亮得像艺术品。于是我就爱上了书写的美，我决定练习书写。
+		但是经过半年的练习，结果比我预期的不理想，对此我感到十分的沮丧，想放弃。
 }
 
