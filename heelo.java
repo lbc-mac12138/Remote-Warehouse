26f2ccc6e21dc3891fb0e48a3d47637734e1d2f4
lbc <3197259460@qq.com>
2022-04-24 10:10:21 +0800

    my update1 commit

--- heelo.java
+++ heelo.java
@@ -3,7 +3,8 @@ public static void main(String[] args){
 
 System.out.println("你好，中国");
 my name is lihua,i am from china;
-fsfsfhjsdfbghjadfsghjfadsghjadfsgjadfshiudfgsijkdfgshiu
+fsfsfhjsdfbghjadfsghjfadsghjadfsgjadfshiudfgsijkdfgshi
 I want to go to school when time at 8:00.
+This is china fly,leave this now,please.
 }
 
