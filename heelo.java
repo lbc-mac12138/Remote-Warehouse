4e40e7b59aafe6c40d8a94fa4733cdc252436d6b
lbc-mac12138 <104205970+lbc-mac12138@users.noreply.github.com>
2022-04-24 09:06:05 +0800

    0424commitfrist

--- heelo.java
+++ heelo.java
@@ -2,26 +2,5 @@ public static void main(String[] args){
 
 System.out.println("你好，中国");
 fsfsfhjsdfbghjadfsghjfadsghjadfsgjadfshiudfgsijkdfgshiu
-
-
-foaeohirhiopaefhiopfadshiopadfshiopadfshijodf
-hu
-	132132132123333333333333333333333333333333333333333333333333
-	whewdezhongguoyikeyebennenfenzge
-	777777-555555555=111111111;;;;;;;
-123333333333333333333333333333333333333
-13333333333333333333333333333333333333333333333333mmmmmmmmmmmm
-
-afhouafehioafijoadfsijoafoiafs:
-13132132131fff313131313131
-9*9=81;
-999999*88888888=fsfsfsfsfs;;;;;;;;
-
-
-	gdshioadghiopudfgshiodfgigsdfhodgs
-		Three years ago, I accidentally realized that my handwriting looks sort of ugly; however, 
-	my best friend’s handwriting is real nice as an art. Then I have a passion for the beauty of handwriting,
-	so I decided to practice calligraphy. But the result of half-year practice was unsatisfied than expecte像艺术品。了书写的美，zzZZZZZZZ我决定练习书写。
-		但是经过半年的练习，结果比我预期的不理想，对此我感到十分的沮丧，想放弃。
 }
 
